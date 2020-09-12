9594e909b3e95cbce514b56e094d383f20a3b85b
Dmitrii Kislenko <kislenko1910@mail.ru>
2020-09-12 17:38:37 +0500

    It sends 3 messages and echos them back

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8082 });
+
+wss.on("connection", ws => {
+    console.log('New client connected');
+    ws.on("message", data => {
+        console.log(`Client's data ${data}`)
+        ws.send(data);
+    })
+    ws.on("close", () => {
+        console.log("Disconnected");
+    })
+});
